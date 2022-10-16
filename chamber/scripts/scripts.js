b39bbb3f69c12077a96172b34d8cf269c67cc547
tulio1990 <trbanegas14@gmail.com>
2022-10-15 23:41:49 -0600

    Update Catracho Site

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -34,3 +34,18 @@ function toggleMenu(){
 
 const x = document.getElementById('hamburgerBtn')
 x.onclick = toggleMenu;
+
+let bannerDay = new Date().getDay();
+
+const banner = document.querySelector('#banner');
+if (bannerDay === 1 || bannerDay === 2){
+    banner.style.display = 'block';
+} else{
+    banner.style.display = 'none';
+}
+
+const close = document.querySelector('#close');
+
+close.addEventListener('click', () => {
+    banner.style.display = 'none';
+})
