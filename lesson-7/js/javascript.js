76dfca2f1830a01c719dd9f3e3da26d105858175
tulio1990 <trbanegas14@gmail.com>
2022-10-27 23:04:27 -0600

    update blur image

--- lesson-7/js/javascript.js
+++ lesson-7/js/javascript.js
@@ -1,11 +1,39 @@
-let imagesToLoad = document.querySelectorAll("img[data-src]");
-const loadImages = (image) => {
-  image.setAttribute("src", image.getAttribute("data-src"));
-  image.onload = () => {
-    image.removeAttribute("data-src");
-  };
+const imagesUpdates = document.querySelectorAll("[data-src]");
+
+function preloadImage(img) {
+    const src = img.getAttribute("data-src");
+    if (!src) {
+        return;
+    }
+    img.onload = () => {
+        img.removeAttribute("data-src");
+    }
+    img.src = src;
+}
+
+
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 10px 0px"
 };
 
-imagesToLoad.forEach((img) => {
-    loadImages(img);
-  });
+const imgShowing = new IntersectionObserver((entries,imgShowing) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            preloadImage(entry.target);
+            imgShowing.unobserve(entry.target);
+        }
+    })
+}, imgOptions);
+
+imagesUpdates.forEach((image) => {
+    imgShowing.observe(image);
+});
+
+
+
+
+
+
