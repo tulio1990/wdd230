92c3809856a87e4c11aee58750591e4289eb8995
tulio1990 <trbanegas14@gmail.com>
2022-10-29 23:55:37 -0600

    update home link

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -102,25 +102,23 @@ visitsDisplay.textContent = `This is your first visit!`;
 numVisits++;
 localStorage.setItem("visits-ls", numVisits);
 
-
-
 const lastVisit = Number(window.localStorage.getItem("last-visit"));
 console.log(lastVisit)
 
-if (lastVisit !== 0) {
+  if (lastVisit !== 0) {
 
-const time = Date.now();
-
-
-let difference = ((time - lastVisit) / (1000 * 60 * 60 * 24)).toFixed(0);
+    const time = Date.now();
 
 
-localStorage.setItem("last-visit", time);
+    let difference = ((time - lastVisit) / (1000 * 60 * 60 * 24)).toFixed(0);
 
 
-lastVisitDisplay.textContent = difference;
+    localStorage.setItem("last-visit", time);
+
+
+    lastVisitDisplay.textContent = difference;
 
 } else {
-visitsDisplay.textContent = `Today is your first day here`;
-localStorage.setItem("last-visit", Date.now());
+  visitsDisplay.textContent = `Today is your first day here`;
+  localStorage.setItem("last-visit", Date.now());
 }
