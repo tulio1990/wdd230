8e203b24ebba44f38c170d01007c2718cfc293d4
tulio1990 <trbanegas14@gmail.com>
2022-12-15 01:38:46 -0600

    options

--- FinalProject/fresh/js/fresh.js
+++ FinalProject/fresh/js/fresh.js
@@ -6,11 +6,21 @@ fetch(requestURL)
     })
     .then(function(jsonObject) {
         console.table(jsonObject); // temporary checking for valid response and data parsing
-        const fruit = jsonObject['']
+        const fruit = jsonObject
         for (let i = 0; i < fruit.length; i++) {
-            let card = Document.getElementById('#genus');
-            let h2 = document.createElement('option');
-            h2.textContent = fruit[i].genus;
-            card.appendChild(h2);
+            let fruitone = document.getElementById('genus');
+            let option = document.createElement('option');
+            option.innerHTML = fruit[i].name;
+            fruitone.appendChild(option);
+
+            let fruittwo = document.getElementById('genus2');
+            let option2 = document.createElement('option');
+            option2.innerHTML = fruit[i].name;
+            fruittwo.appendChild(option2);
+
+            let fruitthree = document.getElementById('genus3');
+            let option3 = document.createElement('option');
+            option3.innerHTML = fruit[i].name;
+            fruitthree.appendChild(option3);
         }
     });
